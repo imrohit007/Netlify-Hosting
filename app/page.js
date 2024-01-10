afedee9c926042a6f22165fdf272d114dbfd170b
Rohit Kumar Thakur <40210878+imrohit007@users.noreply.github.com>
2024-01-10 13:25:33 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
-        This website is hosted on Netlify
+        Like, Share, Subscribe
       </h1>
     </main>
   )
